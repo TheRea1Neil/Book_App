491eba5ccddc429d1b961260442f7092e4d425c1
Neilb <neilb1999@gmail.com>
2024-03-18 18:56:12 -0600

    this is the start

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,12 @@ namespace Book_App.Controllers
     public class HomeController : Controller
     {
 
+        private IBookRepository _repo;
+
+        public HomeController(IBookRepository temp)
+        {
+            _repo = temp;
+        }
         public IActionResult Index()
         {
             return View();
---
+++ Models/IBookRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Book_App.Models
+{
+    public interface IBookRepository
+    {
+        public IQueryable<Book> Books {get;}
+    }
+}
