08d9511a324d39b4b60f744039bcbe839cd94369
Neilb <neilb1999@gmail.com>
2024-03-18 19:58:34 -0600

    asdfasdf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,9 +13,24 @@ namespace Book_App.Controllers
         {
             _repo = temp;
         }
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            int pageSize = 10; // Number of books per page
+            var books = _repo.Books;
+
+            var paginatedBooks = books
+             .OrderBy(b => b.Title) // Order by title or any other property
+             .Skip((page - 1) * pageSize)
+             .Take(pageSize)
+             .ToList();
+
+            int totalBooks = books.Count();
+            int totalPages = (int)Math.Ceiling(totalBooks / (double)pageSize);
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
+            return View(paginatedBooks);
         }
         
     }
